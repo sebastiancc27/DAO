4a424681f047ac00149d42493bf4a304576d37b5
sebastiancc27 <sebasccastizo27@gmail.com>
2023-12-05 15:13:20 -0600

    DAO

--- src/main/java/mx/uv/App.java
+++ src/main/java/mx/uv/App.java
@@ -23,6 +23,48 @@ public class App
         // System.out.println( "Hello World!" );
         // port(80);
 
+        
+        package mx.uv;
+
+import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.UUID;
+import com.google.gson.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    static Gson gson = new Gson();
+    static HashMap<String, Usuario> usuarios = new HashMap<String, Usuario>();
+    
+    public static void main( String[] args )
+    {
+        
+        // System.out.println( "Hello World!" );
+
+        //port(80);
+        port(getHerokuAssignedPort());
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
         get("/usuarios", (request,response)->{
             // return "get";
             // response.type("application/json");
